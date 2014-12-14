205cd549ce03e06152ca4a55d8970422ce76b9b0
subir19mandal <subir.mandal19@yahoo.com>
2014-12-14 12:46:18 +0600

    my project

---
+++ numberUIApp/numberUIApp/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace numberUIApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double anumber = Convert.ToDouble(textBox1.Text);
+            listBox1.Items.Clear();
+            for (int i = 0; i <= anumber; i++)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
